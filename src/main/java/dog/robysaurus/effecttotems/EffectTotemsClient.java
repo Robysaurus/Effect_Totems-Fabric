94b60bb633cece3d2ab5ae2f63d68ed39117cec7
Robysaurus <117132386+Robysaurus@users.noreply.github.com>
2024-12-30 19:41:27 -0800

    Initial commit

---
+++ src/main/java/dog/robysaurus/effecttotems/EffectTotemsClient.java
@@ -0,0 +1,10 @@
+package dog.robysaurus.effecttotems;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EffectTotemsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        
+    }
+}
